123e2f35f12146c46052bc54a966088230038452
Matthew <torry0402@gmail.com>
2024-03-22 21:26:33 +0800

    Update READMD.md

--- Luck Simulator/obj/Debug/net8.0-windows/Luck Simulator.AssemblyInfo.cs
+++ Luck Simulator/obj/Debug/net8.0-windows/Luck Simulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Luck Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c21d97d5971e6aa6ab35f5de938a71a21f1c460")]
 [assembly: System.Reflection.AssemblyProductAttribute("Luck Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Luck Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
